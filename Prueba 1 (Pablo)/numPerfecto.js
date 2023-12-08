950bd09b08d92eb87e74579fea3540d03561d564
Juan Pablo García Villa <juanpablogarciavilla2005@gmai.com>
2023-12-08 15:23:29 -0500

    falling-test

--- Prueba 1 (Pablo)/numPerfecto.js
+++ Prueba 1 (Pablo)/numPerfecto.js
@@ -1,16 +1,6 @@
 function numPerfecto(numero) {
-    if (numero <= 0) {
-      return false;
-    }
-  
-    let sumaDivisores = 0;
-    for (let i = 1; i < numero; i++) {
-      if (numero % i === 0) {
-        sumaDivisores += i;
-      }
-    }
-  
-    return sumaDivisores === numero;
-  }
-  
-  module.exports = numPerfecto;
+  // Cambiando completamente la lógica para que no identifique correctamente los números perfectos
+  return numero % 2 === 0;
+}
+
+module.exports = numPerfecto;
